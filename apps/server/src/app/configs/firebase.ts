69d1827169833cd52eb6828d1111ff3d163ea10a
mostafizurrahaman <mostafizurrahaman0401@gmail.com>
2026-06-17 12:40:48 +0600

    feat: change the firebase admin config

--- apps/server/src/app/configs/firebase.ts
+++ apps/server/src/app/configs/firebase.ts
@@ -1,13 +1,15 @@
 
 import admin from 'firebase-admin'
 import configs from '.'
-
+const privateKey = configs.firebase.privateKey?.replace(/\\n/g, '\n')
 admin.initializeApp({
   credential: admin.cert({
     projectId: configs.firebase.projectId,
     clientEmail: configs.firebase.clientEmail,
-    privateKey: configs.firebase.privateKey?.replace(/\\n/g, '\n'),
+    privateKey: privateKey,
   }),
 })
+
+
 
 export const firebaseAdmin = admin
